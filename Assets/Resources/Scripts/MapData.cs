c070738cc8837dbf9ebda4484d9178d1007eef62
ap0_sec <apo.t.laboratory@gmail.com>
2018-07-16 10:03:34 +0900

    [ADD] this file is manage the state of the stage

---
+++ Assets/Resources/Scripts/MapData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MapData {
+	public State[] z;
+
+	public MapData(int size){
+		z = new State[size];
+		for(int i = 0; i < size; i++){
+			z [i] = new State (size);
+		}
+	}
+}
+
+[Serializable]
+public class State{
+	public int[] x;
+
+	public State(int size){
+		x = new int[size];
+	}
+}
