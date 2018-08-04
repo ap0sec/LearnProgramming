5f3df2f610a436577cba5ec0d96e2263603d0629
ap0_sec <apo.t.laboratory@gmail.com>
2018-08-05 02:12:20 +0900

    [ADD] Goal gimmick + more gimmick Construction

---
+++ Assets/Resources/Scripts/Gimmick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimmick : MonoBehaviour {
+
+	int type;
+	GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		type = 0;
+		switch (transform.tag) {
+		case "Goal":
+			type = 1;
+			break;
+		}
+		cube = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	public void Motions () {
+		switch (type) {
+		case 1:
+			GoalMotion ();
+			break;
+		}
+	}
+
+	void GoalMotion(){
+		Debug.Log ("Goal");
+	}
+
+}
