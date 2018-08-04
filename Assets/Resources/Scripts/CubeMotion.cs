bd1f3926e589c854fe9cb9ff7224d267d0235665
ap0_sec <apo.t.laboratory@gmail.com>
2018-08-05 02:11:09 +0900

    [ADD] wall contact

--- Assets/Resources/Scripts/CubeMotion.cs
+++ Assets/Resources/Scripts/CubeMotion.cs
@@ -8,6 +8,10 @@ public class CubeMotion : MonoBehaviour {
 	Vector3 rotateAxis = Vector3.zero;
 	float cubeAngle = 0f;
 
+	Ray ray;
+	RaycastHit hit;
+	Vector3 pos;
+
 	float cubeSizeHalf;
 	bool isRotate = false;
 
@@ -23,6 +27,9 @@ public class CubeMotion : MonoBehaviour {
 
 	public void MoveCube(int direction){
 		if (isRotate == false) {
+
+
+
 			switch (direction) {
 			case left:
 				rotatePoint = transform.position + new Vector3 (-cubeSizeHalf, -cubeSizeHalf, 0f);
@@ -42,13 +49,28 @@ public class CubeMotion : MonoBehaviour {
 				break;
 			}
 
-			StartCoroutine (RotateCube ());
+			pos = transform.position;
+
+			Debug.Log (pos.x);
+			Debug.Log (pos.y);
+			Debug.Log (pos.z);
+
+			Debug.Log (pos + rotateAxis * (float)0.5);
+
+			ray = new Ray(pos + Quaternion.Euler( 0, -90, 0)*rotateAxis*(float)0.5 , Quaternion.Euler( 0, -90, 0) * rotateAxis);
+
+			if (Physics.Raycast (ray, out hit, (float)0.5)) {
+				Debug.Log ("wall!");
+			} else {
+				StartCoroutine (RotateCube ());
+			}
 
 		}
 	}
 
 
 	IEnumerator RotateCube(){
+		
 		isRotate = true;
 
 		float sumAngle = 0f;
@@ -68,7 +90,12 @@ public class CubeMotion : MonoBehaviour {
 		rotatePoint = Vector3.zero;
 		rotateAxis = Vector3.zero;
 
+		pos = transform.position;
+		ray = new Ray(new Vector3(pos.x,pos.y-(float)0.5,pos.z), new Vector3(0,-1,0));
+
+		if(Physics.Raycast(ray, out hit, (float)0.5)){
+			hit.collider.GetComponent<Gimmick>().Motions ();
+		}
 		yield break;
 	}
-
 }
